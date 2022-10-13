2f3cfed8a88920719984ff73a0f8f05859ea2ab8
Richard-Frost <frost.rchrd.com>
2022-10-13 12:24:01 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Richard Frost
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
