5197ce255b4225279dc42b970ab115c0bd427872
Richard-Frost <frost.rchrd@gmail.com>
2022-10-13 14:18:01 -0700

    Navbar component and css

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
+import React from 'react';
 
+import Navbar from './Navbar';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <p>
-          Richard Frost
-        </p>
+      <Navbar />
       </header>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navbar = () => {
+    return(
+        <div class="nav">
+            <h1>Richard Frost</h1>
+        </div>
+    )
+}
+export default Navbar;
