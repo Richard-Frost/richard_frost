2fa50507f5456fb90788c66cf0314641169a8248
Richard-Frost <frost.rchrd@gmail.com>
2022-11-02 21:00:28 -0700

    bootstrap and css

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Navbar from './Navbar';
 import ProjectLister from './ProjectLister'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 
@@ -10,10 +13,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
+    
       <header className="App-header">
-      <Navbar />
-      <ProjectLister />
+        <Navbar />
       </header>
+
+      <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" id="myVideo">
+        <source src="images/video2.mp4" type="video/mp4"/>
+      </video>  
+      
+      <Container>
+        <ProjectLister />
+      </Container>
+      
+      
     </div>
   );
 }
--- src/components/ProjectLister.js
+++ src/components/ProjectLister.js
@@ -3,29 +3,39 @@ import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image'
 import Card from 'react-bootstrap/Card';
 import Figure from 'react-bootstrap/Figure';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const ProjectLister = () => {
     return(
-        <div class="project">
+         
+        <div>
             {projects.map((project) => { 
                 return(
                 <div>
-                    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={project.image} />
-      <Card.Body>
-        <Card.Title>{project.name}</Card.Title>
-        <Card.Text>
-          {project.about}
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-        <br></br>
-        <a href={project.link}>Github</a><br />
-      </Card.Body>
-    </Card>
+                    <Card className='card-transparent'>
+                    <Card.Img variant="top" src={project.image} />
+                    <Card.Body className='custom-card'>
+                      <Card.Title>{project.name}</Card.Title>
+                      <Card.Text>
+                        {project.about}
+                      </Card.Text>
+                      <Button variant="primary">Go somewhere</Button>
+                      <br></br>
+                      <a href={project.link}>Github</a><br />
+                    </Card.Body>
+                    </Card>
+                    <br/>
+                  
 
     </div>)    
             })}
         </div>
+        
+        
+
+       
     )
 }
 export default ProjectLister;
